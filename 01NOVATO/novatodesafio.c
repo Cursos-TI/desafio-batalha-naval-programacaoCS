e26c59d029f8540dda8393e3a9af1af56ef35c13
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 20:40:57 +0000

    Função para imprimir o Tabuleiro

---
+++ 01NOVATO/novatodesafio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Função para imprimir o Tabuleiro
+
+void imprimirTabuleiro(int tabuleiro[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}

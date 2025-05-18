7a995818f0cec6df899f94a3060857b48ce9bfc6
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 20:50:16 +0000

    Função para imprimir o Tabuleiro

---
+++ 02AVENTUREIRO/aventureirodesafio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Função para imprimir o Tabuleiro
+
+void imprimirTabuleiro(int tabuleiro[10][10]) {
+    printf("  A B C D E F G H I J\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
